d2607ca6018838c749a63131faefd70fb872127f
Win <synwe@yahoo.com>
2019-05-10 13:23:30 +0000

    Update the example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sqlcool/sqlcool.dart';
 import 'pages/select_bloc.dart';
 import 'pages/index.dart';
 import 'pages/join_query.dart';
@@ -14,38 +15,28 @@ void main() {
 }
 
 Future<void> initDb() async {
-  /// these queries will run only once, after the Sqlite file creation
-  String q1 = """CREATE TABLE product (
-      id INTEGER PRIMARY KEY,
-      name VARCHAR(60) NOT NULL,
-      price REAL NOT NULL,
-      category_id INTEGER NOT NULL,
-      CONSTRAINT category
-        FOREIGN KEY (category_id) 
-        REFERENCES category(id) 
-        ON DELETE CASCADE
-      )""";
-  String q2 = """CREATE TABLE category (
-      id INTEGER PRIMARY KEY,
-      name VARCHAR(60) NOT NULL
-      )""";
-  String q3 = 'CREATE UNIQUE INDEX idx_product_name ON product (name)';
-  // populate the database
-  String q4 = 'INSERT INTO category(name) VALUES("Category 1")';
-  String q5 = 'INSERT INTO category(name) VALUES("Category 2")';
-  String q6 = 'INSERT INTO category(name) VALUES("Category 3")';
-  String q7 =
-      'INSERT INTO product(name,price,category_id) VALUES("Product 1", 50, 1)';
-  String q8 =
-      'INSERT INTO product(name,price,category_id) VALUES("Product 2", 30, 1)';
-  String q9 =
-      'INSERT INTO product(name,price,category_id) VALUES("Product 3", 20, 2)';
+  // define the tables
+  DbTable category = DbTable("category")..varchar("name", unique: true);
+  DbTable product = DbTable("product")
+    ..varchar("name", unique: true)
+    ..integer("price")
+    ..foreignKey("category", onDelete: OnDelete.cascade)
+    ..index("name");
+  // prepare the queries
+  List<String> schemaQueries = category.queries..addAll(product.queries);
+  List<String> populateQueries = <String>[
+    'INSERT INTO category(name) VALUES("Category 1")',
+    'INSERT INTO category(name) VALUES("Category 2")',
+    'INSERT INTO category(name) VALUES("Category 3")',
+    'INSERT INTO product(name,price,category_id) VALUES("Product 1", 50, 1)',
+    'INSERT INTO product(name,price,category_id) VALUES("Product 2", 30, 1)',
+    'INSERT INTO product(name,price,category_id) VALUES("Product 3", 20, 2)'
+  ];
+  List<String> queries = schemaQueries..addAll(populateQueries);
+  // initialize the database
   String dbpath = "items.sqlite";
   await db
-      .init(
-          path: dbpath,
-          queries: [q1, q2, q3, q4, q5, q6, q7, q8, q9],
-          verbose: true)
+      .init(path: dbpath, queries: queries, verbose: true)
       .catchError((dynamic e) {
     throw ("Error initializing the database: ${e.message}");
   });
