c61904250af2c70c30a6a0da06668f07553d3d94
Win <synwe@yahoo.com>
2019-07-08 15:25:20 +0000

    Add more tests

--- test/db_test.dart
+++ test/db_test.dart
@@ -10,9 +10,19 @@ void main() async {
   final List<MethodCall> log = <MethodCall>[];
   String response;
   channel.setMockMethodCallHandler((MethodCall methodCall) async {
+    //print("METHOD CALL: $methodCall");
     log.add(methodCall);
     if (methodCall.method == 'getDatabasesPath') {
       return directory.path;
+    } else if (methodCall.method == "insert") {
+      return 1;
+    } else if (methodCall.method == "update") {
+      return 1;
+    } else if (methodCall.method == "query") {
+      final res = <Map<String, dynamic>>[
+        <String, dynamic>{"k": "v"}
+      ];
+      return res;
     }
     return response;
   });
@@ -20,6 +30,9 @@ void main() async {
   tearDown(() {
     log.clear();
   });
+
+  final db = Db();
+
   group("init", () {
     test("Init db", () async {
       final String schema = """CREATE TABLE item
@@ -32,20 +45,95 @@ void main() async {
         'INSERT INTO item(name) values("Name 2")',
         'INSERT INTO item(name) values("Name 3")',
       ];
-      final db = Db();
+
       await db.init(
           path: "testdb.sqlite", absolutePath: true, queries: q, verbose: true);
       expect(db.isReady, true);
+      return true;
     });
 
-    /*test("Init null db", () async {
-      var db = Db();
-      Future<void> init() async {
-        await db.init(
-            path: null, absolutePath: true, queries: [], verbose: true);
+    test("Insert", () async {
+      Future<int> insert() async {
+        final res =
+            await db.insert(table: "test", row: {"k": "v"}, verbose: true);
+        return res;
       }
 
-      expect(init, throwsException);
-    });*/
+      final dynamic res = await insert();
+      print("RES $res / ${res.runtimeType}");
+      return insert().then((int r) => expect(r, 1));
+    });
+
+    test("Delete", () async {
+      Future<int> delete() async {
+        final res =
+            await db.delete(table: "test", where: "id=1", verbose: true);
+        return res;
+      }
+
+      return delete().then((int r) => expect(r, 1));
+    });
+
+    test("Update", () async {
+      Future<int> update() async {
+        final res = await db.update(
+            table: "test", where: "id=1", row: {"k": "v"}, verbose: true);
+        return res;
+      }
+
+      return update().then((int r) => expect(r, 1));
+    });
+
+    test("Upsert", () async {
+      Future<Null> upsert() async {
+        final Null res =
+            await db.upsert(table: "test", row: {"k": "v"}, verbose: true);
+        return res;
+      }
+
+      return upsert().then((r) => expect(r, null));
+    });
+
+    test("Select", () async {
+      Future<List<Map<String, dynamic>>> select() async {
+        final res =
+            await db.select(table: "test", where: "id=1", verbose: true);
+        return res;
+      }
+
+      final output = <Map<String, dynamic>>[
+        <String, dynamic>{"k": "v"}
+      ];
+      return select().then((List<Map<String, dynamic>> r) => expect(r, output));
+    });
+
+    test("Join", () async {
+      Future<List<Map<String, dynamic>>> join() async {
+        final res = await db.join(
+            table: "test",
+            joinOn: "table1.id=1",
+            joinTable: "table1",
+            where: "id=1",
+            verbose: true);
+        return res;
+      }
+
+      final output = <Map<String, dynamic>>[
+        <String, dynamic>{"k": "v"}
+      ];
+      return join().then((List<Map<String, dynamic>> r) => expect(r, output));
+    });
+
+    test("Query", () async {
+      Future<List<Map<String, dynamic>>> query() async {
+        final res = await db.query("SELECT * from test_table");
+        return res;
+      }
+
+      final output = <Map<String, dynamic>>[
+        <String, dynamic>{"k": "v"}
+      ];
+      return query().then((List<Map<String, dynamic>> r) => expect(r, output));
+    });
   });
 }
