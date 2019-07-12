fd3152dfee2adf0049c8f2e80ae462d0644c8b7c
Win <synwe@yahoo.com>
2019-07-12 16:26:01 +0000

    Minor fix in tests

--- test/db_test.dart
+++ test/db_test.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/services.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:sqlcool/sqlcool.dart';
 
 void main() async {
