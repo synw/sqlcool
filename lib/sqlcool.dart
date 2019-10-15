08258dee79d12c49234dcd6c73af48c96287d09c
synw <ansterj@gmail.com>
2019-10-15 21:29:27 +0200

    Export exceptions

--- lib/sqlcool.dart
+++ lib/sqlcool.dart
@@ -8,3 +8,4 @@ export 'src/schema/models/schema.dart';
 export 'src/schema/models/table.dart';
 export 'src/schema/models/column.dart';
 export 'src/schema/models/dbmodels.dart';
+export 'src/exceptions.dart';
