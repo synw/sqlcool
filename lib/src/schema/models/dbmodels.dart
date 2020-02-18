c71bbe486a0e2099b7874e7aa9e1dc57c142a795
synw <synw@yahoo.com>
2020-02-18 08:50:09 +0100

    Refactor DbModel.sqlJoin

--- lib/src/schema/models/dbmodels.dart
+++ lib/src/schema/models/dbmodels.dart
@@ -1,7 +1,9 @@
 import 'package:sqlcool/src/exceptions.dart';
+import 'package:flutter/foundation.dart';
 
 import '../../database.dart';
 import 'table.dart';
+import 'column.dart';
 
 /// The database model class to extend
 class DbModel {
@@ -35,82 +37,87 @@ class DbModel {
       String groupBy,
       bool verbose = false}) async {
     _checkDbIsReady();
-    final colStringsSelect = <String>["${table.name}.id AS id"];
-    // get regular column names
-    for (final col in table.columns) {
-      if (!col.isForeignKey) {
-        final encodedName = "${table.name}.${col.name} AS ${col.name}";
-        colStringsSelect.add(encodedName);
-      }
-    }
-    // build up the joins from schema
-    final joinsTables = <String>[];
-    final joinsOn = <String>[];
-    final fkColStringsSelect = <String>[];
-    final fkPropertiesCols = <String, Map<String, String>>{};
+    print("> Sqljoin for table $table");
+    //table.describe();
+    final _joinTables = <String>[];
+    final _joinOn = <String>[];
+    final _select = <String>[];
+    final _encodedFks = <_EncodedFk>[];
+    table.columns
+      ..add(const DbColumn(name: "id", type: DbColumnType.integer))
+      ..forEach((c) {
+        if (!c.isForeignKey) {
+          _select.add("${table.name}.${c.name} AS ${c.name}");
+        }
+      });
     for (final fkCol in table.foreignKeys) {
-      final ref = fkCol?.reference ?? fkCol.name;
-      joinsTables.add(ref);
-      joinsOn.add("${table.name}.${fkCol.name}=$ref.id");
-      // grab the foreign key table schema
-      final fkTable = db.schema.table(ref);
-      // get columns for foreign key
-      final fkColsNames =
-          fkTable.columns.map<String>((col) => col.name).toList()..add("id");
-      //for (final fc in fkTable.columns) {
-      for (final fc in fkColsNames) {
-        // encode for select
-        final endName = "${ref}_$fc";
-        final encodedFkName = "$ref.$fc AS $endName";
-        fkColStringsSelect.add(encodedFkName);
-        fkPropertiesCols[endName] = <String, String>{
-          "col_name": fc,
-          "fk_name": fkCol.name
-        };
-        //print("Encoded fk ${fkPropertiesCols[endName]}");
+      final fkTable = db.schema.table(fkCol.reference);
+      _joinTables.add(fkTable.name);
+      //print("FK COLS $fkTable: ${fkTable.columns}");
+      final c = fkTable.columns
+        ..add(const DbColumn(name: "id", type: DbColumnType.integer));
+      //print("NEW FK COLS ${fkTable.columns}");
+      _joinOn.add("${table.name}.${fkCol.name}=${fkTable.name}.id");
+      //print("Joins add ${table.name}.${fkCol.name}=${fkTable.name}.id");
+      for (final _fkTableCol in c) {
+        final encodedName = "${fkTable.name}_${_fkTableCol.name}";
+        final fk = _EncodedFk(
+            table: fkTable,
+            name: _fkTableCol.name,
+            encodedName: encodedName,
+            refColName: fkCol.name);
+        _encodedFks.add(fk);
+        final encodedFkName = "${fkTable.name}.${fk.name} AS $encodedName";
+        _select.add(encodedFkName);
       }
     }
-    colStringsSelect.addAll(fkColStringsSelect);
-    final columns = colStringsSelect.join(",");
+    final columns = _select.join(",");
     final res = await db.mJoin(
         table: table.name,
-        joinsTables: joinsTables,
-        joinsOn: joinsOn,
+        joinsTables: _joinTables,
+        joinsOn: _joinOn,
         columns: columns,
         offset: offset,
         limit: limit,
         where: where,
         groupBy: groupBy,
         verbose: verbose);
-    // encode foreign keys results into dict for proper
-    // decoding with client .fromDb methods
-    final fres = <Map<String, dynamic>>[];
+    final endRes = <Map<String, dynamic>>[];
+    //print("\nRES $res\n");
     for (final row in res) {
-      final newRow = <String, dynamic>{};
+      final endRow = <String, dynamic>{};
       final fkData = <String, Map<String, dynamic>>{};
-      // set fk data keys
-      String ref;
-      for (final c in table.foreignKeys) {
-        ref = c?.reference ?? c.name;
-        fkData[ref] = <String, dynamic>{};
-      }
-      // retrieve data
-      row.forEach((String k, dynamic v) {
-        if (fkPropertiesCols.keys.contains(k)) {
-          fkData[ref][fkPropertiesCols[k]["col_name"]] = v;
-          // decode foreign key name from select results
-          newRow[fkPropertiesCols[k]["fk_name"]] = fkData[ref];
+      row.forEach((key, dynamic value) {
+        final encodedFk =
+            _encodedFks.where((element) => element.encodedName == key).toList();
+        if (encodedFk.isEmpty) {
+          // it is not a foreign key
+          endRow[key] = value;
         } else {
-          newRow[k] = v;
+          final efk = encodedFk[0];
+          //print("EFK $efk");
+          if (!fkData.containsKey(efk.refColName)) {
+            fkData[efk.refColName] = <String, dynamic>{};
+          }
+          fkData[efk.refColName][efk.name] = value;
+          //endRow[key][encodedFk[0].refColName] = value;
+          //print("FKDATA : $fkData");
         }
       });
-      fres.add(newRow);
+      for (final c in fkData.keys) {
+        //print("FK DATA $c : ${fkData[c]}");
+        endRow[c] = fkData[c];
+      }
+      //print("END ROW $endRow");
+      endRes.add(endRow);
     }
-    final endRes = <dynamic>[];
-    for (final row in fres) {
-      endRes.add(fromDb(row));
+    //print("QUERY END RES: $endRes");
+    final endModelData = <dynamic>[];
+    for (final r in endRes) {
+      endModelData.add(fromDb(r));
     }
-    return endRes;
+    //print("End model data: $endModelData");
+    return endModelData;
   }
 
   /// Select rows in the database table
@@ -234,5 +241,29 @@ class DbModel {
     assert(table != null);
     assert(db != null);
     assert(db.isReady, "Please intialize the database by running db.init()");
+  }
+}
+
+class _EncodedFk {
+  const _EncodedFk(
+      {@required this.table,
+      @required this.name,
+      @required this.encodedName,
+      @required this.refColName});
+
+  final DbTable table;
+  final String name;
+  final String encodedName;
+  final String refColName;
+
+  @override
+  String toString() {
+    final s = StringBuffer()
+      ..write("Encoded fk:")
+      ..write("\n- Table: $table")
+      ..write("\n- Name: $name")
+      ..write("\n- Encoded name: $encodedName")
+      ..write("\n- Ref col name: $refColName");
+    return s.toString();
   }
 }
